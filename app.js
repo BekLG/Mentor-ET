f8d2bf9e4d0c80bf5bf5878ccb38b80664909d44
BekLg <bereketlegesse22@gmail.com>
2023-04-18 11:16:58 +0300

    imported necessary packages

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("the node JS file");
+require("dotenv").config();
+const express = require("express");
+const ejs= require("ejs");
+const bodyParser= require("body-parser");
+const mongoose= require("mongoose");
+const lodash= require("lodash");
+
+const app= express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req,res){
+        res.render("home");
+})
