55e34c45b2978a6390c24ffb2e070272e1094ebb
BekLg <bereketlegesse22@gmail.com>
2023-05-24 16:08:06 +0300

    formated the app.js code

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const ejs= require("ejs");
-const bodyParser= require("body-parser");
-const mongoose= require("mongoose");
-const lodash= require("lodash");
-const session= require("express-session");
-const passport= require("passport");
-const passportLocalMongoose= require("passport-local-mongoose");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const lodash = require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require('mongoose-findorcreate');
 
-const app= express();
+const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
@@ -24,13 +24,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const dbUsername= process.env.DB_USERNAME;
-const dbPassword= process.env.DB_PASSWORD;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
 
-mongoose.connect("mongodb+srv://" + dbUsername + ":" + dbPassword + "@cluster1.8xjo7em.mongodb.net/mentorET", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://" + dbUsername + ":" + dbPassword + "@cluster1.8xjo7em.mongodb.net/mentorET", { useNewUrlParser: true });
 
 
-const mentorSchema= new mongoose.Schema({
+const mentorSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     educationLevel: String,     // degree,masters, undergraduate....
@@ -38,13 +38,13 @@ const mentorSchema= new mongoose.Schema({
     profession: String, //sewyew currently eyesera yalebet position like accountant, fullstack developer...
     email: String,
     password: String,
-    profileCompleted:{type: Boolean, default: false }
+    profileCompleted: { type: Boolean, default: false }
 });
 
 mentorSchema.plugin(passportLocalMongoose);
 mentorSchema.plugin(findOrCreate);
 
-const Mentor= new mongoose.model("mentor", mentorSchema);
+const Mentor = new mongoose.model("mentor", mentorSchema);
 
 passport.use(Mentor.createStrategy());
 
@@ -60,163 +60,159 @@ passport.deserializeUser((mentorId, done) => {
 });
 
 
-const articleSchema= new mongoose.Schema({
+const articleSchema = new mongoose.Schema({
     title: String,
     fields: [String],
     content: String,
     author: String,
     motive: String,
     datePublished: String,
-    approved: {type: Boolean, default: false }
+    approved: { type: Boolean, default: false }
 });
 
-const Article= new mongoose.model("article", articleSchema);
+const Article = new mongoose.model("article", articleSchema);
 
-const fieldSchema= new mongoose.Schema({
+const fieldSchema = new mongoose.Schema({
     name: String,
-    category: [String] 
+    category: [String]
 });
 
-const Field= new mongoose.model("field", fieldSchema);
+const Field = new mongoose.model("field", fieldSchema);
 
-const emailSubscriberSchema= new mongoose.Schema({
+const emailSubscriberSchema = new mongoose.Schema({
     email: String
 });
 
-const Email= new mongoose.model("email", emailSubscriberSchema);
+const Email = new mongoose.model("email", emailSubscriberSchema);
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
 
-        Article.find({approved:true}).limit(3) // select only 3 articles,       selecting criterea will be modified.
-    .then((foundArticle)=>{
-        Field.find({})  //fetch all fields
-        .then((foundField)=>{
-            res.render("home",{Articles: foundArticle, Fields: foundField, isLoggedIn: req.session.isLoggedIn});
+    Article.find({ approved: true }).limit(3) // select only 3 articles,       selecting criterea will be modified.
+        .then((foundArticle) => {
+            Field.find({})  //fetch all fields
+                .then((foundField) => {
+                    res.render("home", { Articles: foundArticle, Fields: foundField, isLoggedIn: req.session.isLoggedIn });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err);
-        })  
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+        })
 })
 
-app.get("/signInUp", function(req,res){
+app.get("/signInUp", function (req, res) {
     // this page will send a page that contains both a signin and signup pages with their forms
     res.render("mentor")
 
     // from the sign in and up page there will be two possible post requests /login and /register
 })
-app.post("/login", function(req,res){
+app.post("/login", function (req, res) {
     // handle login process here
-    const mentor= new Mentor({
+    const mentor = new Mentor({
         username: req.body.username,
         password: req.body.password
     });
 
-    req.login(mentor, function(err){
-        if(err)
-        {
+    req.login(mentor, function (err) {
+        if (err) {
             console.log(err);
         }
-        else{
-            passport.authenticate("local")(req, res, function(){
+        else {
+            passport.authenticate("local")(req, res, function () {
                 req.session.isLoggedIn = true;
 
 
                 // check if the users profile is completed, if not completed redirect user to complete profile page. if completed redirect user to home page
 
                 Mentor.findOne({ _id: req.user._id })
-                .then((foundMentor)=>{
-                    if(foundMentor.profileCompleted===false)
-                    {
-                        console.log("please complete your profile");
-                        res.redirect("/completeProfile");
-                    }
-                    else
-                    {
-                        res.redirect("/")
-                    }
-                })
-                .catch((err)=>{
-                    console.log(err);
-                })
+                    .then((foundMentor) => {
+                        if (foundMentor.profileCompleted === false) {
+                            console.log("please complete your profile");
+                            res.redirect("/completeProfile");
+                        }
+                        else {
+                            res.redirect("/")
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
             })
         }
     })
 })
 
-app.get("/logout", function(req,res){
-    req.logout(function(err){
-        if(err)
-        {
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
             console.log(err);
         }
         res.redirect("/");
     });
-    
+
 });
 
 
-app.post("/register", function(req,res){
+app.post("/register", function (req, res) {
     // handle registration process here
-   
-    Mentor.register({username : req.body.username}, req.body.password, function(err, user){
-        if(!err){
-            passport.authenticate("local")(req, res, function(){
+
+    Mentor.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (!err) {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/completeProfile")
             })
-        } else{
+        } else {
             console.log(err);
         }
     })
 })
 
-app.get("/completeProfile", function(req,res){
+app.get("/completeProfile", function (req, res) {
     // a route to ask the mentor to fill relevant informations about him....after signing up.
     res.render("completeprofile");
 })
 
-app.post("/completeProfile", function(req,res){
+app.post("/completeProfile", function (req, res) {
     const query = { _id: req.user._id };
-    Mentor.findOneAndUpdate(query, { 
+    Mentor.findOneAndUpdate(query, {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        educationLevel: req.body.educationLevel,     
-        fieldOfExpertise: req.body.fieldOfExpertise,    
+        educationLevel: req.body.educationLevel,
+        fieldOfExpertise: req.body.fieldOfExpertise,
         profession: req.body.profession,
         profileCompleted: true
-     })
-     .then(()=>{
-        res.redirect("/");
-     })
-     .catch((err)=>{
-        console.log(err);
-     })       
+    })
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
 
-app.get("/composeArticle", function(req,res){
+app.get("/composeArticle", function (req, res) {
     // check if the user is mentor
 
     Field.find({})  //fetch all fields
-    .then((foundField)=>{
-        res.render("composeArticle",{ Fields: foundField});
-    })
-    .catch((err)=>{
-        console.log(err);
-    })  
+        .then((foundField) => {
+            res.render("composeArticle", { Fields: foundField });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
-app.post("/composeArticle", function(req,res){
+app.post("/composeArticle", function (req, res) {
     const currentDate = new Date();
-    const authorName= req.user.firstName + " " + req.user.lastName;     // getting looged user's full name
+    const authorName = req.user.firstName + " " + req.user.lastName;     // getting looged user's full name
 
-    const article= new Article({
+    const article = new Article({
         title: req.body.title,
         fields: req.body.fields,
         content: req.body.content,
-        author: authorName,  
-        motive: req.body.motive,   
+        author: authorName,
+        motive: req.body.motive,
         datePublished: currentDate
     })
 
@@ -225,89 +221,89 @@ app.post("/composeArticle", function(req,res){
     res.redirect("/");
 })
 
-app.get("/articles",function(req,res){
-    Article.find({approved:true}) // filter articles that are approved by admin from database
-    .then((foundArticle)=>{
-            
-        Field.find({})  //fetch all fields
-        .then((foundField)=>{
-            res.render("blog",{Articles: foundArticle, Fields: foundField, isLoggedIn: req.session.isLoggedIn});
+app.get("/articles", function (req, res) {
+    Article.find({ approved: true }) // filter articles that are approved by admin from database
+        .then((foundArticle) => {
+
+            Field.find({})  //fetch all fields
+                .then((foundField) => {
+                    res.render("blog", { Articles: foundArticle, Fields: foundField, isLoggedIn: req.session.isLoggedIn });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err);
-        })  
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-})
-
-
-app.get("/articles/:field", function(req,res){
-    const field= req.params.field;
-   
-    Article.find({fields: field, approved:true}) // filter articles with selected fields
-    .then((foundArticle)=>{
-
-        Field.find({})  //fetch all fields
-        .then((foundField)=>{
-            res.render("blog",{Articles: foundArticle, Fields: foundField, isLoggedIn: req.session.isLoggedIn});
         })
-        .catch((err)=>{
-            console.log(err);
-        })  
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-})
-app.get("/articles/read/:articleId", function(req,res){     //a route for read more, to display the whole article in ine page.
-    const articleId= req.params.articleId;
-    Article.find({_id: articleId})
-    .then((foundArticle)=>{ 
-        res.render("readMore",{article: foundArticle}); 
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-
 })
 
-app.get("/admin", function(red,res){
-    Article.find({approved:false}) // filter articles that are not approved yet
-    .then((foundArticle)=>{
 
-        Field.find({})  //fetch all fields
-        .then((foundField)=>{
-            res.render("admin",{Articles: foundArticle, Fields: foundField});
+app.get("/articles/:field", function (req, res) {
+    const field = req.params.field;
+
+    Article.find({ fields: field, approved: true }) // filter articles with selected fields
+        .then((foundArticle) => {
+
+            Field.find({})  //fetch all fields
+                .then((foundField) => {
+                    res.render("blog", { Articles: foundArticle, Fields: foundField, isLoggedIn: req.session.isLoggedIn });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err);
-        })  
+        })
+})
+app.get("/articles/read/:articleId", function (req, res) {     //a route for read more, to display the whole article in ine page.
+    const articleId = req.params.articleId;
+    Article.find({ _id: articleId })
+        .then((foundArticle) => {
+            res.render("readMore", { article: foundArticle });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 
-        
-       
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
 })
 
-app.post("/admin/approvePost", function(req,res){
+app.get("/admin", function (red, res) {
+    Article.find({ approved: false }) // filter articles that are not approved yet
+        .then((foundArticle) => {
+
+            Field.find({})  //fetch all fields
+                .then((foundField) => {
+                    res.render("admin", { Articles: foundArticle, Fields: foundField });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+
+
+
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.post("/admin/approvePost", function (req, res) {
     const query = { _id: req.body.articleId };
-    Article.findOneAndUpdate(query, { 
-        approved:true
-     })
-     .then(()=>{
-        res.redirect("/admin");
-     })
-     .catch((err)=>{
-        console.log(err);
-     })  
+    Article.findOneAndUpdate(query, {
+        approved: true
+    })
+        .then(() => {
+            res.redirect("/admin");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
 
-app.post("/admin/addField", function(req,res){
-    const field= new Field({
+app.post("/admin/addField", function (req, res) {
+    const field = new Field({
         name: req.body.name,
         category: req.body.category
     })
@@ -315,15 +311,15 @@ app.post("/admin/addField", function(req,res){
     res.redirect("/admin");
 })
 
-app.post("/subscribeEmail", function(req,res){
-    const email= new Email({
-        email:req.body.emailAdress
+app.post("/subscribeEmail", function (req, res) {
+    const email = new Email({
+        email: req.body.emailAdress
     })
     email.save();
     res.redirect("/");
 })
 
 
-app.listen(3000, function(req,res){
+app.listen(3000, function (req, res) {
     console.log("server is up and running on port 3000");
 })
