002d981877c3852c52329ab052851c62608def1c
BekLg <bereketlegesse22@gmail.com>
2023-05-28 07:50:27 +0300

    configured paths of public and views directories

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const path = require("path");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
